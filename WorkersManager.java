ec92c2d6f3e43eda7f8507da0fc978494c7140d2
nictruong <nic.truong@gmail.com>
2017-05-08 14:14:13 -0400

    changed how buildTile is found

--- WorkersManager.java
+++ WorkersManager.java
@@ -43,13 +43,16 @@ public class WorkersManager {
 	
 	public Boolean build(UnitType unitType) {
 		Unit freeWorker = getWorker();
-		TilePosition buildTile = getBuildTile(freeWorker, unitType, self.getStartLocation());
 		
-		for (QueuedBuildingUnit queuedBuilding : buildingQueue) {
+		
+		
+		/*for (QueuedBuildingUnit queuedBuilding : buildingQueue) {
 			if (queuedBuilding.getLocation().getX() == buildTile.getX() && queuedBuilding.getLocation().getY() == buildTile.getY()) {
 				return false;
 			}
-		}
+		}*/
+		
+		TilePosition buildTile = game.getBuildLocation(unitType, freeWorker.getTilePosition());
 		
 		if (freeWorker.build(unitType, buildTile)) {
 			buildingQueue.add(new QueuedBuildingUnit(unitType, freeWorker, buildTile));
@@ -116,43 +119,5 @@ public class WorkersManager {
 		}
 		
 		return null;
-	}
-	
-	public TilePosition getBuildTile(Unit builder, UnitType buildingType, TilePosition aroundTile) {
-		TilePosition ret = null;
-		int maxDist = 2;
-		int stopDist = 40;
-
-		// Refinery, Assimilator, Extractor
-		if (buildingType.isRefinery()) {
-			for (Unit n : this.game.neutral().getUnits()) {
-				if ((n.getType() == UnitType.Resource_Vespene_Geyser) &&
-						( Math.abs(n.getTilePosition().getX() - aroundTile.getX()) < stopDist ) &&
-						( Math.abs(n.getTilePosition().getY() - aroundTile.getY()) < stopDist )
-						) return n.getTilePosition();
-			}
-		}
-
-		while ((maxDist < stopDist) && (ret == null)) {
-			for (int i=aroundTile.getX()-maxDist; i<=aroundTile.getX()+maxDist; i++) {
-				for (int j=aroundTile.getY()-maxDist; j<=aroundTile.getY()+maxDist; j++) {
-					if (this.game.canBuildHere(new TilePosition(i,j), buildingType, builder, false)) {
-						// units that are blocking the tile
-						boolean unitsInWay = false;
-						for (Unit u : this.game.getAllUnits()) {
-							if (u.getID() == builder.getID()) continue;
-							if ((Math.abs(u.getTilePosition().getX()-i) < 4) && (Math.abs(u.getTilePosition().getY()-j) < 4)) unitsInWay = true;
-						}
-						if (!unitsInWay) {
-							return new TilePosition(i, j);
-						}
-					}
-				}
-			}
-			maxDist += 2;
-		}
-
-		if (ret == null) this.game.printf("Unable to find suitable build position for "+buildingType.toString());
-		return ret;
 	}
 }
